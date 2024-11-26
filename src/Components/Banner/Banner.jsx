9ffa78e8172d57435f841d20ff93a22fe548da0c
zillulofficial <zillul.hakim100@gmail.com>
2024-11-26 12:17:27 +0600

    banner section added

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,34 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+const Banner = () => {
+    return (
+        <Carousel
+            infiniteLoop
+            autoPlay
+            interval={2500}
+            className="text-center"
+        >
+            <div className="lg:h-[800px]">
+                <img src="https://i.postimg.cc/RZfDQj3d/01.jpg" />
+            </div>
+            <div className="lg:h-[800px]">
+                <img src="https://i.postimg.cc/xjxh0GDv/02.jpg" />
+            </div>
+            <div className="lg:h-[800px]">
+                <img src="https://i.postimg.cc/xTX7qknW/03.png" />
+            </div>
+            <div className="lg:h-[800px]">
+                <img src="https://i.postimg.cc/02h4S8Nr/04.jpg" />
+            </div>
+            <div className="lg:h-[800px]">
+                <img src="https://i.postimg.cc/GpCSMqGC/05.png" />
+            </div>
+            <div className="lg:h-[800px]">
+                <img src="https://i.postimg.cc/R0Lp2KLn/06.png" />
+            </div>
+        </Carousel>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Banner from "../../Components/Banner/Banner";
 
 const Home = () => {
     return (
         <div>
-            this is Home
+            <Banner></Banner>
         </div>
     );
 };
